nickguletskii/OpenOlympus

commit 01d5e6b9c2777d32306c8d153572d4d1b861766b

Use $http instead of Restangular since it doesn't offer any benefits

src/main/resources/resources/js/controllers/admin/pendingUsers.js
@@ -22,84 +22,81 @@
  */
 define(['oolutil', 'lodash'],
     function(Util, _) {
-        return ['$timeout', '$q', '$scope', '$rootScope', '$http', '$location',
-            '$stateParams', 'Restangular',
-            function($timeout, $q, $scope, $rootScope, $http, $location,
-                $stateParams, Restangular) {
+        return function($timeout, $q, $scope, $rootScope, $http, $location,
+            $stateParams) {
 
-                $scope.$apply(function() {
-                    var page = $stateParams.page;
+            $scope.$apply(function() {
+                var page = $stateParams.page;
 
-                    $scope.page = $stateParams.page;
+                $scope.page = $stateParams.page;
 
-                    function updateUsers() {
-                        Restangular.all('api/admin/pendingUsers').getList({
-                            page: page
-                        }).then(function(users) {
-                            $scope.users = users;
-                        });
-                        $http.get('api/admin/pendingUsersCount').then(function(response) {
-                            $scope.userCount = response.data;
-                        });
-                    }
-                    updateUsers();
+                function updateUsers() {
+                    $http.get('api/admin/pendingUsers', {
+                        page: page
+                    }).success(function(users) {
+                        $scope.users = users;
+                    });
+                    $http.get('api/admin/pendingUsersCount').then(function(response) {
+                        $scope.userCount = response.data;
+                    });
+                }
+                updateUsers();
 
-                    function handleApprovalResponse(userApprovals) {
-                        Restangular.all('api/admin/pendingUsers').getList({
-                            page: page
-                        }).then(function(users) {
-                            $scope.users = _.map(users, function(user) {
-                                var oldUser = _.find($scope.users, function(oldUser) {
-                                    return oldUser.id == user.id;
-                                });
-                                if (!!oldUser)
-                                    user = oldUser;
-                                var userResponse = _.find(userApprovals, function(userResponse) {
-                                    return userResponse.id == user.id;
-                                });
-                                if (!userResponse)
-                                    return user;
-                                return _.assign(user, {
-                                    "checked": true,
-                                    "error": userResponse.statusMessage,
-                                });
+                function handleApprovalResponse(userApprovals) {
+                    $http.get('api/admin/pendingUsers', {
+                        page: page
+                    }).success(function(users) {
+                        $scope.users = _.map(users, function(user) {
+                            var oldUser = _.find($scope.users, function(oldUser) {
+                                return oldUser.id == user.id;
                             });
-                            $scope.loading = false;
+                            if (!!oldUser)
+                                user = oldUser;
+                            var userResponse = _.find(userApprovals, function(userResponse) {
+                                return userResponse.id == user.id;
+                            });
+                            if (!userResponse)
+                                return user;
+                            return _.assign(user, {
+                                "checked": true,
+                                "error": userResponse.statusMessage,
+                            });
                         });
-                        $http.get('api/admin/pendingUsersCount').then(function(response) {
-                            $scope.userCount = response.data;
-                        });
-                    }
-                    $scope.approveUsers = function() {
-                        $scope.loading = true;
-                        $http.post("/api/admin/users/approve", _($scope.users).filter(function(user) {
-                            return user.checked;
-                        }).map(function(user) {
-                            return user.id;
-                        }).value()).success(handleApprovalResponse);
-                    };
+                        $scope.loading = false;
+                    });
+                    $http.get('api/admin/pendingUsersCount').then(function(response) {
+                        $scope.userCount = response.data;
+                    });
+                }
+                $scope.approveUsers = function() {
+                    $scope.loading = true;
+                    $http.post("/api/admin/users/approve", _($scope.users).filter(function(user) {
+                        return user.checked;
+                    }).map(function(user) {
+                        return user.id;
+                    }).value()).success(handleApprovalResponse);
+                };
 
 
-                    $scope.retryApprovingFailedUsers = function() {
-                        $scope.loading = true;
-                        $http.post("/api/admin/users/approve", _($scope.users).filter(function(user) {
-                            return !!user.error;
-                        }).map(function(user) {
-                            return user.id;
-                        }).value()).success(handleApprovalResponse);
-                    };
+                $scope.retryApprovingFailedUsers = function() {
+                    $scope.loading = true;
+                    $http.post("/api/admin/users/approve", _($scope.users).filter(function(user) {
+                        return !!user.error;
+                    }).map(function(user) {
+                        return user.id;
+                    }).value()).success(handleApprovalResponse);
+                };
 
 
-                    $scope.deleteUsersWithErrors = function() {
-                        $http.post("/api/admin/users/deleteUsers", _($scope.users).filter(function(user) {
-                            return user.checked && !!user.error;
-                        }).map(function(user) {
-                            return user.id;
-                        }).value()).success(function() {
-                            updateUsers();
-                        });
-                    };
-                });
-            }
-        ];
+                $scope.deleteUsersWithErrors = function() {
+                    $http.post("/api/admin/users/deleteUsers", _($scope.users).filter(function(user) {
+                        return user.checked && !!user.error;
+                    }).map(function(user) {
+                        return user.id;
+                    }).value()).success(function() {
+                        updateUsers();
+                    });
+                };
+            });
+        };
     });

src/main/resources/resources/js/controllers/archive/tasks.js
@@ -22,25 +22,22 @@
  */
 define(['oolutil', 'lodash'],
     function(Util, _) {
-        return ['$timeout', '$q', '$scope', '$rootScope', '$http', '$location',
-            '$stateParams', 'Restangular',
-            function($timeout, $q, $scope, $rootScope, $http, $location,
-                $stateParams, Restangular) {
-                $scope.$apply(function() {
-                    var page = $stateParams.page;
+        return function($timeout, $q, $scope, $rootScope, $http, $location,
+            $stateParams) {
+            $scope.$apply(function() {
+                var page = $stateParams.page;
 
-                    $scope.page = $stateParams.page;
+                $scope.page = $stateParams.page;
 
-                    Restangular.all('api/archive/tasks').getList({
-                        page: page
-                    }).then(function(tasks) {
-                        $scope.tasks = tasks;
-                    });
-
-                    $http.get('api/archive/taskCount').then(function(response) {
-                        $scope.taskCount = response.data;
-                    });
+                $http.get('api/archive/tasks', {
+                    page: page
+                }).success(function(tasks) {
+                    $scope.tasks = tasks;
                 });
-            }
-        ];
+
+                $http.get('api/archive/taskCount').then(function(response) {
+                    $scope.taskCount = response.data;
+                });
+            });
+        };
     });

src/main/resources/resources/js/controllers/archive/users.js
@@ -22,25 +22,22 @@
  */
 define(['oolutil', 'lodash'],
     function(Util, _) {
-        return ['$timeout', '$q', '$scope', '$rootScope', '$http', '$location',
-            '$stateParams', 'Restangular',
-            function($timeout, $q, $scope, $rootScope, $http, $location,
-                $stateParams, Restangular) {
+        return function($timeout, $q, $scope, $rootScope, $http, $location,
+            $stateParams) {
 
-                $scope.$apply(function() {
-                    var page = $stateParams.page;
+            $scope.$apply(function() {
+                var page = $stateParams.page;
 
-                    $scope.page = $stateParams.page;
+                $scope.page = $stateParams.page;
 
-                    Restangular.all('api/archive/rank').getList({
-                        page: page
-                    }).then(function(users) {
-                        $scope.users = users;
-                    });
-                    $http.get('api/archive/rankCount').then(function(response) {
-                        $scope.userCount = response.data;
-                    });
+                $http.get('api/archive/rank', {
+                    page: page
+                }).success(function(users) {
+                    $scope.users = users;
                 });
-            }
-        ];
+                $http.get('api/archive/rankCount').then(function(response) {
+                    $scope.userCount = response.data;
+                });
+            });
+        };
     });

src/main/resources/resources/js/controllers/contests.js
@@ -22,25 +22,22 @@
  */
 define(['oolutil', 'lodash'],
     function(Util, _) {
-        return ['$timeout', '$q', '$scope', '$rootScope', '$http', '$location',
-            '$stateParams', 'Restangular',
-            function($timeout, $q, $scope, $rootScope, $http, $location,
-                $stateParams, Restangular) {
-                $scope.$apply(function() {
-                    var page = $stateParams.page;
+        return function($timeout, $q, $scope, $rootScope, $http, $location,
+            $stateParams) {
+            $scope.$apply(function() {
+                var page = $stateParams.page;
 
-                    $scope.page = $stateParams.page;
+                $scope.page = $stateParams.page;
 
-                    Restangular.all('api/contests/').getList({
-                        page: page
-                    }).then(function(contests) {
-                        $scope.contests = contests;
-                    });
-
-                    $http.get('api/contestsCount').then(function(response) {
-                        $scope.contestsCount = response.data;
-                    });
+                $http.get('api/contests/', {
+                    page: page
+                }).success(function(contests) {
+                    $scope.contests = contests;
                 });
-            }
-        ];
+
+                $http.get('api/contestsCount').then(function(response) {
+                    $scope.contestsCount = response.data;
+                });
+            });
+        };
     });

src/main/resources/resources/js/controllers/contests/contest/participants.js
@@ -22,25 +22,22 @@
  */
 define(['oolutil', 'lodash'],
     function(Util, _) {
-        return ['$timeout', '$q', '$scope', '$rootScope', '$http', '$location',
-            '$stateParams', 'Restangular',
-            function($timeout, $q, $scope, $rootScope, $http, $location,
-                $stateParams, Restangular) {
+        return function($timeout, $q, $scope, $rootScope, $http, $location,
+            $stateParams) {
 
-                $scope.$apply(function() {
-                    var page = $stateParams.page;
+            $scope.$apply(function() {
+                var page = $stateParams.page;
 
-                    $scope.page = $stateParams.page;
+                $scope.page = $stateParams.page;
 
-                    Restangular.all('api/contest/'+$stateParams.contestId+"/participants").getList({
-                        page: page
-                    }).then(function(users) {
-                        $scope.users = users;
-                    });
-                    $http.get('api/contest/'+$stateParams.contestId+'/participantsCount').then(function(response) {
-                        $scope.userCount = response.data;
-                    });
+                $http.get('api/contest/' + $stateParams.contestId + "/participants", {
+                    page: page
+                }).success(function(users) {
+                    $scope.users = users;
                 });
-            }
-        ];
+                $http.get('api/contest/' + $stateParams.contestId + '/participantsCount').then(function(response) {
+                    $scope.userCount = response.data;
+                });
+            });
+        };
     });

src/main/resources/resources/js/controllers/contests/contest/results.js
@@ -22,28 +22,25 @@
  */
 define(['oolutil', 'lodash'],
     function(Util, _) {
-        return ['$timeout', '$q', '$scope', '$rootScope', '$http', '$location',
-            '$stateParams', 'Restangular',
-            function($timeout, $q, $scope, $rootScope, $http, $location,
-                $stateParams, Restangular) {
+        return function($timeout, $q, $scope, $rootScope, $http, $location,
+            $stateParams) {
 
-                $scope.$apply(function() {
-                    var page = $stateParams.page;
+            $scope.$apply(function() {
+                var page = $stateParams.page;
 
-                    $scope.page = $stateParams.page;
+                $scope.page = $stateParams.page;
 
-                    Restangular.all('api/contest/' + $stateParams.contestId + "/results").getList({
-                        page: page
-                    }).then(function(users) {
-                        $scope.users = users;
-                        $scope.tasks = _.map(users[0].taskScores, function(taskScore) {
-                            return taskScore.first;
-                        });
-                    });
-                    $http.get('api/contest/' + $stateParams.contestId + '/participantsCount').then(function(response) {
-                        $scope.userCount = response.data;
+                $http.get('api/contest/' + $stateParams.contestId + "/results", {
+                    page: page
+                }).success(function(users) {
+                    $scope.users = users;
+                    $scope.tasks = _.map(users[0].taskScores, function(taskScore) {
+                        return taskScore.first;
                     });
                 });
-            }
-        ];
+                $http.get('api/contest/' + $stateParams.contestId + '/participantsCount').then(function(response) {
+                    $scope.userCount = response.data;
+                });
+            });
+        };
     });

src/main/resources/resources/js/controllers/user/solutions.js
@@ -22,25 +22,22 @@
  */
 define(['oolutil', 'lodash'],
     function(Util, _) {
-        return ['$timeout', '$q', '$scope', '$rootScope', '$http', '$location',
-            '$stateParams', 'Restangular',
-            function($timeout, $q, $scope, $rootScope, $http, $location,
-                $stateParams, Restangular) {
+        return function($timeout, $q, $scope, $rootScope, $http, $location,
+            $stateParams) {
 
-                $scope.$apply(function() {
-                    var page = $stateParams.page;
+            $scope.$apply(function() {
+                var page = $stateParams.page;
 
-                    $scope.page = $stateParams.page;
+                $scope.page = $stateParams.page;
 
-                    Restangular.all('api/user/solutions').getList({
-                        page: page
-                    }).then(function(solutions) {
-                        $scope.solutions = solutions;
-                    });
-                    $http.get('api/user/solutionsCount').then(function(response) {
-                        $scope.solutionCount = response.data;
-                    });
+                $http.get('api/user/solutions', {
+                    page: page
+                }).success(function(solutions) {
+                    $scope.solutions = solutions;
                 });
-            }
-        ];
+                $http.get('api/user/solutionsCount').then(function(response) {
+                    $scope.solutionCount = response.data;
+                });
+            });
+        };
     });

src/main/resources/resources/js/controllers/archive/tasks/add.js
@@ -23,7 +23,7 @@
 define(['oolutil', 'lodash'],
     function(Util, _) {
         return function($timeout, $q, $scope, $rootScope, $http,
-            $location, $stateParams, Restangular, $state, AuthenticationProvider, ServersideFormErrorReporter, ValidationService, $upload) {
+            $location, $stateParams, $state, AuthenticationProvider, ServersideFormErrorReporter, ValidationService, $upload) {
             $scope.$apply(function() {
                 $scope.serverErrorReporter = new ServersideFormErrorReporter();
                 $scope.taskCreationForm.forceValidation = true;

commit 40ac8062821cdfc20590afadca3195879efb94df

Fix controllers broken in 01d5e6b9c2777d32306c8d153572d4d1b861766b

src/main/resources/resources/js/controllers/admin/pendingUsers.js
@@ -32,7 +32,9 @@ define(['oolutil', 'lodash'],
 
                 function updateUsers() {
                     $http.get('api/admin/pendingUsers', {
-                        page: page
+                        params: {
+                            page: page
+                        }
                     }).success(function(users) {
                         $scope.users = users;
                     });
@@ -44,7 +46,9 @@ define(['oolutil', 'lodash'],
 
                 function handleApprovalResponse(userApprovals) {
                     $http.get('api/admin/pendingUsers', {
-                        page: page
+                        params: {
+                            page: page
+                        }
                     }).success(function(users) {
                         $scope.users = _.map(users, function(user) {
                             var oldUser = _.find($scope.users, function(oldUser) {

src/main/resources/resources/js/controllers/archive/tasks.js
@@ -30,7 +30,9 @@ define(['oolutil', 'lodash'],
                 $scope.page = $stateParams.page;
 
                 $http.get('api/archive/tasks', {
-                    page: page
+                    params: {
+                        page: page
+                    }
                 }).success(function(tasks) {
                     $scope.tasks = tasks;
                 });

src/main/resources/resources/js/controllers/archive/users.js
@@ -31,7 +31,9 @@ define(['oolutil', 'lodash'],
                 $scope.page = $stateParams.page;
 
                 $http.get('api/archive/rank', {
-                    page: page
+                    params: {
+                        page: page
+                    }
                 }).success(function(users) {
                     $scope.users = users;
                 });

src/main/resources/resources/js/controllers/contests.js
@@ -30,7 +30,9 @@ define(['oolutil', 'lodash'],
                 $scope.page = $stateParams.page;
 
                 $http.get('api/contests/', {
-                    page: page
+                    params: {
+                        page: page
+                    }
                 }).success(function(contests) {
                     $scope.contests = contests;
                 });

src/main/resources/resources/js/controllers/contests/contest/participants.js
@@ -31,7 +31,9 @@ define(['oolutil', 'lodash'],
                 $scope.page = $stateParams.page;
 
                 $http.get('api/contest/' + $stateParams.contestId + "/participants", {
-                    page: page
+                    params: {
+                        page: page
+                    }
                 }).success(function(users) {
                     $scope.users = users;
                 });

src/main/resources/resources/js/controllers/contests/contest/results.js
@@ -31,7 +31,9 @@ define(['oolutil', 'lodash'],
                 $scope.page = $stateParams.page;
 
                 $http.get('api/contest/' + $stateParams.contestId + "/results", {
-                    page: page
+                    params: {
+                        page: page
+                    }
                 }).success(function(users) {
                     $scope.users = users;
                     $scope.tasks = _.map(users[0].taskScores, function(taskScore) {

src/main/resources/resources/js/controllers/user/solutions.js
@@ -31,7 +31,9 @@ define(['oolutil', 'lodash'],
                 $scope.page = $stateParams.page;
 
                 $http.get('api/user/solutions', {
-                    page: page
+                    params: {
+                        page: page
+                    }
                 }).success(function(solutions) {
                     $scope.solutions = solutions;
                 });