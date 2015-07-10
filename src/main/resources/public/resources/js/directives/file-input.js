61c684d477113fc5850eb61cd59ac3bb652401e6
Nick Guletskii <nick@nickguletskii.com>
2015-07-10 21:53:30 +0300

    Fix file input being eaten by Angular, improve styling

--- src/main/resources/public/resources/js/directives/file-input.js
+++ src/main/resources/public/resources/js/directives/file-input.js
@@ -1,23 +1,37 @@
 var $ = require("jquery");
 var angular = require("angular");
-var app = require("app");
 
-module.exports = /*@ngInject*/ function(FieldHelper, $http, $compile, $timeout) {
+module.exports = /*@ngInject*/ function(FieldHelper) {
 	return {
-		restrict: 'E',
-		require: '^formFor',
+		restrict: "E",
+		require: "^formFor",
 		template: require("ng-cache!directives/file-input.html"),
 		scope: {
-			attribute: '@',
-			label: '@',
-			multiple: '@'
+			attribute: "@",
+			label: "@",
+			multiple: "@"
 		},
 		link: function($scope, $element, $attributes, formForController) {
 			FieldHelper.manageFieldRegistration($scope, $attributes, formForController);
-
-			$scope.$watch($attributes.multiple, function(val) {
+			let bindableFix = $scope.$watch(() => $scope.model.bindable, function() {
+				Object.defineProperty($scope.model, "bindable", {
+					set: (value) => {
+						//Hide bindable from Angular's shitty built-in validation that somehow casts the files to string.
+						if (angular.isString(value)) {
+							return;
+						}
+						this.hideYoBindable = value;
+					},
+					get: () => this.hideYoBindable
+				});
+			});
+			let watcher = $scope.$watch($attributes.multiple, function(val) {
 				$($element[0]).find(".directive-main-element").attr("multiple", val);
+			});
+			$scope.$on("$destroy", () => {
+				watcher();
+				bindableFix();
 			});
 		}
 	};
-}
+};
