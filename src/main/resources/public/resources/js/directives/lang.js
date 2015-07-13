7fb162c132ca92fa5bbcd01e36f8573c53b7b60d
Nick Guletskii <nick@nickguletskii.com>
2015-07-13 14:47:26 +0300

    Improve directive registration

--- src/main/resources/public/resources/js/directives/lang.js
+++ src/main/resources/public/resources/js/directives/lang.js
@@ -1,21 +1,20 @@
 var angular = require("angular");
-var app = require("app");
 
-module.exports = /*@ngInject*/ function($compile, $translate, $rootScope) {
-    return {
-        restrict: "AE",
-        link: function(scope, element, attrs, ctrl, transclude) {
-            var name = attrs.lang;
+angular.module("ool.directives").directive("lang", /*@ngInject*/ function($compile, $translate, $rootScope) {
+	return {
+		restrict: "AE",
+		link: function(scope, element, attrs) {
+			var name = attrs.lang;
 
-            function changeTranslation() {
-                if (name && name !== $rootScope.currentLanguage) {
-                    element[0].hidden = true;
-                } else {
-                    element[0].hidden = false;
-                }
-            }
-            changeTranslation();
-            $rootScope.$watch("currentLanguage", changeTranslation);
-        }
-    };
-}
+			function changeTranslation() {
+				if (name && name !== $rootScope.currentLanguage) {
+					element[0].hidden = true;
+				} else {
+					element[0].hidden = false;
+				}
+			}
+			changeTranslation();
+			$rootScope.$watch("currentLanguage", changeTranslation);
+		}
+	};
+});
